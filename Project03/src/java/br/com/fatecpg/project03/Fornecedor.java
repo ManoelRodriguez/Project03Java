0a35667d7bad8e0a0b09230bd2b105226031ae4d
Manoel Victor <manoel.rodriguez98@gmail.com>
2019-03-28 13:26:06 -0300

    Class Fornecedor

---
+++ Project03/src/java/br/com/fatecpg/project03/Fornecedor.java
@@ -0,0 +1,61 @@
+package br.com.fatecpg.project03;
+
+public class Fornecedor {
+
+    private String nome;
+    private String razao;
+    private String cnpj;
+    private String email;
+    private String telefone;
+    private String endereço;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getRazao() {
+        return razao;
+    }
+
+    public void setRazao(String razao) {
+        this.razao = razao;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEndereço() {
+        return endereço;
+    }
+
+    public void setEndereço(String endereço) {
+        this.endereço = endereço;
+    }
+    
+    
+}
