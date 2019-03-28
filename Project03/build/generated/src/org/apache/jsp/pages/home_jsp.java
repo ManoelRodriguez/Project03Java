017b4ecc7b6d4eccf628533f86f3e8617efac44a
Manoel Victor <manoel.rodriguez98@gmail.com>
2019-03-28 13:19:37 -0300

    Home

---
+++ Project03/build/generated/src/org/apache/jsp/pages/home_jsp.java
@@ -0,0 +1,166 @@
+package org.apache.jsp.pages;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.jsp.*;
+
+public final class home_jsp extends org.apache.jasper.runtime.HttpJspBase
+    implements org.apache.jasper.runtime.JspSourceDependent {
+
+  private static final JspFactory _jspxFactory = JspFactory.getDefaultFactory();
+
+  private static java.util.List<String> _jspx_dependants;
+
+  private org.glassfish.jsp.api.ResourceInjector _jspx_resourceInjector;
+
+  public java.util.List<String> getDependants() {
+    return _jspx_dependants;
+  }
+
+  public void _jspService(HttpServletRequest request, HttpServletResponse response)
+        throws java.io.IOException, ServletException {
+
+    PageContext pageContext = null;
+    HttpSession session = null;
+    ServletContext application = null;
+    ServletConfig config = null;
+    JspWriter out = null;
+    Object page = this;
+    JspWriter _jspx_out = null;
+    PageContext _jspx_page_context = null;
+
+    try {
+      response.setContentType("text/html;charset=UTF-8");
+      pageContext = _jspxFactory.getPageContext(this, request, response,
+      			null, true, 8192, true);
+      _jspx_page_context = pageContext;
+      application = pageContext.getServletContext();
+      config = pageContext.getServletConfig();
+      session = pageContext.getSession();
+      out = pageContext.getOut();
+      _jspx_out = out;
+      _jspx_resourceInjector = (org.glassfish.jsp.api.ResourceInjector) application.getAttribute("com.sun.appserv.jsp.resource.injector");
+
+      out.write("\n");
+      out.write("\n");
+      out.write("\n");
+      out.write("<!DOCTYPE html>\n");
+      out.write("<html lang=\"en\">\n");
+      out.write("\n");
+      out.write("<head>\n");
+      out.write("    <meta charset=\"UTF-8\">\n");
+      out.write("    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n");
+      out.write("    <link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css\"\n");
+      out.write("        integrity=\"sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO\" crossorigin=\"anonymous\">\n");
+      out.write("\n");
+      out.write("    <title>Home</title>\n");
+      out.write("\n");
+      out.write("</head>\n");
+      out.write("\n");
+      out.write("<body>\n");
+      out.write("\n");
+      out.write("    <nav class=\"navbar navbar-expand-lg navbar-light bg-dark\">\n");
+      out.write("        <a class=\"navbar-brand\" href=\"home.jsp\" style=\"color: white\">Projeto 3</a>\n");
+      out.write("        <button class=\"navbar-toggler\" type=\"button\" data-toggle=\"collapse\" data-target=\"#conteudoNavbarSuportado\"\n");
+      out.write("            aria-controls=\"conteudoNavbarSuportado\" aria-expanded=\"false\" aria-label=\"Alterna navegação\">\n");
+      out.write("            <span class=\"navbar-toggler-icon\"></span>\n");
+      out.write("        </button>\n");
+      out.write("\n");
+      out.write("        <div class=\"collapse navbar-collapse\" id=\"conteudoNavbarSuportado\">\n");
+      out.write("            <ul class=\"navbar-nav mr-auto\">\n");
+      out.write("                <li class=\"nav-item active\">\n");
+      out.write("                    <a class=\"nav-link\" href=\"home.jsp\" style=\"color: white\">Home <span class=\"sr-only\">(página\n");
+      out.write("                            atual)</span></a>\n");
+      out.write("                </li>\n");
+      out.write("                <li class=\"nav-item\">\n");
+      out.write("                    <a class=\"nav-link\" href=\"clientes.jsp\" style=\"color: white\">Clientes</a>\n");
+      out.write("                </li>\n");
+      out.write("\n");
+      out.write("                <li class=\"nav-item\">\n");
+      out.write("                    <a class=\"nav-link disabled\" href=\"fornecedores.jsp\" style=\"color: white\">Fornecedores</a>\n");
+      out.write("                </li>\n");
+      out.write("                <li class=\"nav-item dropdown\">\n");
+      out.write("                    <a class=\"nav-link dropdown-toggle\" href=\"#\" id=\"navbarDropdown\" role=\"button\"\n");
+      out.write("                        data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"false\" style=\"color: white;\">\n");
+      out.write("                        Cadastre-se\n");
+      out.write("                    </a>\n");
+      out.write("                    <div class=\"dropdown-menu\" aria-labelledby=\"navbarDropdown\">\n");
+      out.write("                        <a class=\"dropdown-item\" href=\"cadastro-cliente.jsp\" style=\"color: black;\">Clientes</a>\n");
+      out.write("                        <a class=\"dropdown-item\" href=\"cadastro-fornecedor.jsp\" style=\"color: black;\">Fornecedores</a>\n");
+      out.write("                        <div class=\"dropdown-divider\"></div>\n");
+      out.write("                    </div>\n");
+      out.write("                </li>\n");
+      out.write("            </ul>\n");
+      out.write("            <form class=\"form-inline my-2 my-lg-0\">\n");
+      out.write("                <input class=\"form-control mr-sm-2\" type=\"search\" placeholder=\"Pesquisar\" aria-label=\"Pesquisar\">\n");
+      out.write("                <button class=\"btn btn-outline-primary my-2 my-sm-0\" type=\"submit\">Pesquisar</button>\n");
+      out.write("            </form>\n");
+      out.write("        </div>\n");
+      out.write("    </nav>\n");
+      out.write("    <div class=\"jumbotron\" style=\"height: 350px; padding: 120px;\">\n");
+      out.write("        <div class=\"container\">\n");
+      out.write("            <h1>Informações</h1>\n");
+      out.write("            <p>Este projeto consiste em uma aplicação Java Web para cadastro de Clientes e Fornecedores, utilizando ArrayList.\n");
+      out.write("            </p>\n");
+      out.write("        </div>\n");
+      out.write("    </div>\n");
+      out.write("    <div class=\"container\">\n");
+      out.write("        <div class=\"row\">\n");
+      out.write("            <div class=\"col-md-6\" style=\"text-align: justify;\">\n");
+      out.write("                <h3>Cadastro para Clientes</h3>\n");
+      out.write("                <ul>\n");
+      out.write("                    <li>Nome</li>\n");
+      out.write("                    <li>CPF</li>\n");
+      out.write("                    <li>RG</li>\n");
+      out.write("                    <li>Email</li>\n");
+      out.write("                    <li>Telefone</li>\n");
+      out.write("                    <li>Endereço</li>\n");
+      out.write("                </ul>\n");
+      out.write("            </div>\n");
+      out.write("            <div class=\"col-md-6\" style=\"text-align: justify;\">\n");
+      out.write("                <h3>Cadastro para Fornecedores</h3>\n");
+      out.write("                <ul>\n");
+      out.write("                    <li>Nome</li>\n");
+      out.write("                    <li>Razão Social</li>\n");
+      out.write("                    <li>CNPJ</li>\n");
+      out.write("                    <li>Email</li>\n");
+      out.write("                    <li>Telefone</li>\n");
+      out.write("                    <li>Endereço</li>\n");
+      out.write("                </ul>\n");
+      out.write("            </div>\n");
+      out.write("\n");
+      out.write("        </div>\n");
+      out.write("\n");
+      out.write("    </div>\n");
+      out.write("    <footer>\n");
+      out.write("        <hr>\n");
+      out.write("        <p style=\"text-align: center\"> <a href=\"https://github.com/ManoelRodriguez\">Manoel Victor</a> || <a\n");
+      out.write("                href=\"https://github.com/matheussmorais\">Matheus Morais</a></p>\n");
+      out.write("        <!-- JavaScript (Opcional) -->\n");
+      out.write("        <!-- jQuery primeiro, depois Popper.js, depois Bootstrap JS -->\n");
+      out.write("        <script src=\"https://code.jquery.com/jquery-3.3.1.slim.min.js\"\n");
+      out.write("            integrity=\"sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo\" crossorigin=\"anonymous\">\n");
+      out.write("        </script>\n");
+      out.write("        <script src=\"https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js\"\n");
+      out.write("            integrity=\"sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49\" crossorigin=\"anonymous\">\n");
+      out.write("        </script>\n");
+      out.write("        <script src=\"https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js\"\n");
+      out.write("            integrity=\"sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy\" crossorigin=\"anonymous\">\n");
+      out.write("        </script>\n");
+      out.write("    </footer>\n");
+      out.write("</body>\n");
+      out.write("\n");
+      out.write("</html>\n");
+    } catch (Throwable t) {
+      if (!(t instanceof SkipPageException)){
+        out = _jspx_out;
+        if (out != null && out.getBufferSize() != 0)
+          out.clearBuffer();
+        if (_jspx_page_context != null) _jspx_page_context.handlePageException(t);
+        else throw new ServletException(t);
+      }
+    } finally {
+      _jspxFactory.releasePageContext(_jspx_page_context);
+    }
+  }
+}
---
+++ Project03/build/web/pages/home.jsp
@@ -0,0 +1,114 @@
+<%-- 
+    Document   : home
+    Created on : 28/03/2019, 13:12:19
+    Author     : Manoel Rodriguez
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <title>Home</title>
+
+</head>
+
+<body>
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-dark">
+        <a class="navbar-brand" href="home.jsp" style="color: white">Projeto 3</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#conteudoNavbarSuportado"
+            aria-controls="conteudoNavbarSuportado" aria-expanded="false" aria-label="Alterna navegação">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="conteudoNavbarSuportado">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="home.jsp" style="color: white">Home <span class="sr-only">(página
+                            atual)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="clientes.jsp" style="color: white">Clientes</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link disabled" href="fornecedores.jsp" style="color: white">Fornecedores</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="color: white;">
+                        Cadastre-se
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="cadastro-cliente.jsp" style="color: black;">Clientes</a>
+                        <a class="dropdown-item" href="cadastro-fornecedor.jsp" style="color: black;">Fornecedores</a>
+                        <div class="dropdown-divider"></div>
+                    </div>
+                </li>
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Pesquisar" aria-label="Pesquisar">
+                <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Pesquisar</button>
+            </form>
+        </div>
+    </nav>
+    <div class="jumbotron" style="height: 350px; padding: 120px;">
+        <div class="container">
+            <h1>Informações</h1>
+            <p>Este projeto consiste em uma aplicação Java Web para cadastro de Clientes e Fornecedores, utilizando ArrayList.
+            </p>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6" style="text-align: justify;">
+                <h3>Cadastro para Clientes</h3>
+                <ul>
+                    <li>Nome</li>
+                    <li>CPF</li>
+                    <li>RG</li>
+                    <li>Email</li>
+                    <li>Telefone</li>
+                    <li>Endereço</li>
+                </ul>
+            </div>
+            <div class="col-md-6" style="text-align: justify;">
+                <h3>Cadastro para Fornecedores</h3>
+                <ul>
+                    <li>Nome</li>
+                    <li>Razão Social</li>
+                    <li>CNPJ</li>
+                    <li>Email</li>
+                    <li>Telefone</li>
+                    <li>Endereço</li>
+                </ul>
+            </div>
+
+        </div>
+
+    </div>
+    <footer>
+        <hr>
+        <p style="text-align: center"> <a href="https://github.com/ManoelRodriguez">Manoel Victor</a> || <a
+                href="https://github.com/matheussmorais">Matheus Morais</a></p>
+        <!-- JavaScript (Opcional) -->
+        <!-- jQuery primeiro, depois Popper.js, depois Bootstrap JS -->
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+            integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+        </script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+            integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous">
+        </script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+            integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous">
+        </script>
+    </footer>
+</body>
+
+</html>
