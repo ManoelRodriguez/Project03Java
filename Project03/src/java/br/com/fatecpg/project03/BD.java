ff03c71ac46f981aa2bf521c078a0cbad872fdc2
Manoel Victor <manoel.rodriguez98@gmail.com>
2019-03-28 13:46:51 -0300

    Array List

---
+++ Project03/src/java/br/com/fatecpg/project03/BD.java
@@ -0,0 +1,24 @@
+package br.com.fatecpg.project03;
+
+import java.util.ArrayList;
+
+public class BD {
+
+    private static ArrayList<Fornecedor> fornecedoresList;
+
+    public static ArrayList<Fornecedor> getContatosList() {
+        if (fornecedoresList == null) {
+            fornecedoresList = new ArrayList<>();
+            Fornecedor f1 = new Fornecedor();
+            f1.setNome("Manoel Victor");
+            f1.setRazao("Transporte de cargas");
+            f1.setCnpj("12.345.678/9123-45");
+            f1.setEmail("manoel@gmail.com");
+            f1.setTelefone("+55 13 99123-4567");
+            f1.setEndereço("Vila São Jorge - Santos");
+
+        }
+        return fornecedoresList;
+    }
+
+}
