3264c768babb652513d8e7efdbcfa1e9bbf3d9ca
Manoel Victor <manoel.rodriguez98@gmail.com>
2019-03-31 15:47:10 -0300

    Acrescentando dados na tabela

--- Project03/src/java/br/com/fatecpg/project03/BD.java
+++ Project03/src/java/br/com/fatecpg/project03/BD.java
@@ -11,12 +11,30 @@ public class BD {
             fornecedoresList = new ArrayList<>();
             Fornecedor f1 = new Fornecedor();
             f1.setNome("Manoel Victor");
-            f1.setRazao("Transporte de cargas");
+            f1.setRazao("Cargas Express");
             f1.setCnpj("12.345.678/9123-45");
             f1.setEmail("manoel@gmail.com");
             f1.setTelefone("+55 13 99123-4567");
-            f1.setEndereco("Vila São Jorge - Santos");
+            f1.setEndereco("Santos");
             fornecedoresList.add(f1);
+            
+            Fornecedor f2 = new Fornecedor();
+            f2.setNome("Daniel Ramos");
+            f2.setRazao("World Devs");
+            f2.setCnpj("17.953.678/973-45");
+            f2.setEmail("daniel@gmail.com");
+            f2.setTelefone("+55 13 97983-4567");
+            f2.setEndereco("Praia Grande");
+            fornecedoresList.add(f2);
+            
+            Fornecedor f3 = new Fornecedor();
+            f3.setNome("Emerson Silva");
+            f3.setRazao("Planets Students");
+            f3.setCnpj("45.983.68/973-75");
+            f3.setEmail("emerson@gmail.com");
+            f3.setTelefone("+55 13 97983-8321");
+            f3.setEndereco("São Paulo");
+            fornecedoresList.add(f3);
         }
         return fornecedoresList;
     }
@@ -34,6 +52,24 @@ public class BD {
             novoc.setTel("+55 13 99123-1234");
             novoc.setEndereco("Praça 19 de Janeiro, 144 - Praia Grande");
             clientesList.add(novoc);
+            
+            Cliente novoc2 = new Cliente();
+            novoc2.setNome("Michael Pires");
+            novoc2.setCpf("777.888.999-75");
+            novoc2.setRegistrogeral("99.555.111-7");
+            novoc2.setEmail("michael@yahoo.com");
+            novoc2.setTel("+55 13 97896-1278");
+            novoc2.setEndereco("Guarujá");
+            clientesList.add(novoc2);
+            
+            Cliente novoc3 = new Cliente();
+            novoc3.setNome("Leandro Costa");
+            novoc3.setCpf("789.742.349-95");
+            novoc3.setRegistrogeral("18.294.783-3");
+            novoc3.setEmail("leandro@yahoo.com");
+            novoc3.setTel("+55 13 97896-1223");
+            novoc3.setEndereco("Bertioga");
+            clientesList.add(novoc3);
 
         }
         return clientesList;
